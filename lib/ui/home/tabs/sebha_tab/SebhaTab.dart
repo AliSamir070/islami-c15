25de61b2e38ead01ea159865ac7505eaa09303a6
Ali070 <Ali.zidan39@yahoo.com>
2025-07-15 21:14:24 +0300

    + finish sebha tab + finish onboarding

--- lib/ui/home/tabs/sebha_tab/SebhaTab.dart
+++ lib/ui/home/tabs/sebha_tab/SebhaTab.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 
 class SebhaTab extends StatelessWidget {
   const SebhaTab({super.key});
-
+  // finish sebha assignment
   @override
   Widget build(BuildContext context) {
     return Container(color: Colors.black);
